85ac2c722b92b22714692545cc226298848720e6
Uwe Klotz <uwe.klotz@gmail.com>
2023-12-11 03:42:58 +0100

    Move lints into Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,6 @@
 
 //! Immutable, path-addressable tree data structure.
 
-#![warn(rust_2018_idioms)]
-#![warn(rust_2021_compatibility)]
-#![warn(missing_debug_implementations)]
-#![warn(unreachable_pub)]
-#![warn(unsafe_code)]
-#![warn(rustdoc::broken_intra_doc_links)]
-#![warn(clippy::pedantic)]
-// Additional restrictions
-#![warn(clippy::clone_on_ref_ptr)]
-#![warn(clippy::missing_const_for_fn)]
-#![warn(clippy::self_named_module_files)]
 // Repetitions of module/type names occur frequently when using many
 // modules for keeping the size of the source files handy. Often
 // types have the same name as their parent module.
